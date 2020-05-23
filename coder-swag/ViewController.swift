becdfed633d00a461bd6ba660d8b2fad5aa6a85c
Shawn Mansfield <shawn.mansfield@gmail.com>
2020-05-23 05:20:01 -0400

    Initial Commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Shawn Mansfield on 5/23/20.
+//  Copyright © 2020 Shawn Mansfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
